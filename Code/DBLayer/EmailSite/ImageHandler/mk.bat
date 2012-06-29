567f83d0278cb996c4f0b37d6959e1d22fcea4cc
Nguyen <hoangphucnguyen@yahoo.com>
2012-06-29 15:28:24 +0100

    add handler

--- Code/DBLayer/EmailSite/ImageHandler/mk.bat
+++ Code/DBLayer/EmailSite/ImageHandler/mk.bat
@@ -1,2 +1,2 @@
-C:\Windows\Microsoft.NET\Framework\v2.0.50727\csc /t:library /out:ImageHandler.dll ImageHandler.cs /r:System.web.dll  /r:system.dll /r:System.Drawing.dll  /r:/bin/DBLayer.dll
+C:\Windows\Microsoft.NET\Framework\v2.0.50727\csc /t:library /out:ImageHandler.dll ImageHandler.cs /r:System.web.dll  /r:system.dll /r:System.Drawing.dll  /r:../bin/DBLayer.dll
 pause
